396f1d69a9b1a7f5660ff7689456a464b2c043e0
Niklas Fauth <niklasfauth@gmx.de>
2017-11-12 16:35:46 +0100

    ADD: receive odometry from hoverboard

--- sessel_otter/scripts/hardware_driver.py
+++ sessel_otter/scripts/hardware_driver.py
@@ -1,29 +1,51 @@
 #!/usr/bin/env python2
 import rospy
 from geometry_msgs.msg import Twist, Vector3
+from sessel_otter.msg import MotorTicks
 import serial
 import struct
 
 speed = 0
 direction = 0
 timeout = 0
+ser = None
 
 TIMEOUT      = 10
 MAX_SPEED    = 1 # m/s
 MAX_STEERING = 0.1 # rad/s
+
+import threading
+
+
+def receiveSerial ():
+    r = rospy.Rate(250) # Hz
+    global ser
+    pub = rospy.Publisher('motor_ticks', MotorTicks, queue_size = 1)
+    while not rospy.is_shutdown():
+        try:
+            response = ser.readline()
+            ticks = map(int, response.split(';'))
+            #print(ticks[0])
+            #print(ticks[1])
+            pub.publish(MotorTicks(ticks[0], ticks[1]))
+        except Exception as ex:
+            print ex
+            pass
+        r.sleep()
+
 
 def clamp(n, minn, maxn):
     return max(min(maxn, n), minn)
 
 def callback(data):
     global speed, direction, timeout
-    speed = clamp(data.linear.x, -MAX_SPEED, MAX_SPEED)
-    direction = clamp(data.angular.z, -MAX_STEERING, MAX_STEERING)
+    speed = clamp(data.linear.z, -MAX_SPEED, MAX_SPEED)
+    direction = clamp(data.linear.x, -MAX_STEERING, MAX_STEERING)
     timeout = 0
     #rospy.loginfo("Speed: %f", speed)
 
 def main():
-    global speed, direction, timeout
+    global speed, direction, timeout, ser
     rospy.init_node('hardware_driver')
 
     try:
@@ -39,6 +61,9 @@ def main():
     r = rospy.Rate(100) # Hz
     vel_tp = [0] * 50 # 50 sample low-pass for speed
     dir_tp = [0] * 10 # 10 sample low-pass for steering
+
+    thread = threading.Thread(target=receiveSerial, args=[])
+    thread.start()
 
     while not rospy.is_shutdown():
         vel_tp[len(vel_tp)-1] = speed if not timeout > TIMEOUT else 0
@@ -64,9 +89,6 @@ def main():
                 ser.write(b)
             for b in binL:
                 ser.write(b)
-
-        #response = ser.readline()
-        #print(response)
 
         timeout+=1
         r.sleep()
